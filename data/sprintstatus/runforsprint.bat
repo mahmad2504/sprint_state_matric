efc86e04e2e116157a2dffb07455b8755f038717
mahmad2504 <mumtazahmad2504@gmail.com>
2021-07-23 10:18:59 +0500

    updated

---
+++ data/sprintstatus/runforsprint.bat
@@ -0,0 +1,2 @@
+@docker  cp env sprint_state_matric_container:\sprint_state_matric\.env
+@docker exec -it -w /sprint_state_matric sprint_state_matric_container php artisan sprintstatus:sync --sprint=%1 --force=1
---
+++ data/sprintstatus/shell.bat
@@ -0,0 +1,1 @@
+@docker exec -it -w /sprint_state_matric sprint_state_matric_container bash
